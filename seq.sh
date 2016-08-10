df070f9cad9057564d7e978b994d899d920d2cf5
sanjeevtripurari <sanjeevtripurari@users.noreply.github.com>
2016-08-10 05:37:21 +0530

    Update seq.sh

--- seq.sh
+++ seq.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
-echo "Displays count from 1 to 20 added step" 
-for i in `seq 1 20 `; do
+echo "Displays count from 1 to 10 added step" 
+
+for i in `seq 1 10 `; do
  	echo "i:",$i 
 done
